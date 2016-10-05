ecb06f18a34a19651cb568563fc3f992ee842adc
Daryl Watson <darylwtsn@gmail.com>
2016-10-05 15:11:21 +0100

    Basic styling

--- js/views/message.js
+++ js/views/message.js
@@ -2,13 +2,17 @@ var Backchat = Backchat || {};
 
 Backchat.MessageView = Backbone.View.extend({
   tagName: 'li',
+  className: 'message',
 
   template: _.template($('#message-template').html()),
 
   events: {},
 
   render: function() {
-    this.$el.html(this.model.attributes.author + ' - ' + this.model.attributes.content);
+    var attributes = this.model.attributes;
+    this.$el.html(this.template());
+    this.$('.author').text(attributes.author + ':');
+    this.$('.content').text(attributes.content);
     return this;
   }
 });
